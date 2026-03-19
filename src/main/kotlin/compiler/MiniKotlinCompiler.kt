00d98c39d52f0ae28051df6e707d999b3d0972cf
1Tymon1 <tymonbielski@gmail.com>
2026-03-19 18:42:38 +0100

    initial commit

--- src/main/kotlin/compiler/MiniKotlinCompiler.kt
+++ src/main/kotlin/compiler/MiniKotlinCompiler.kt
@@ -6,6 +6,11 @@ import MiniKotlinParser
 class MiniKotlinCompiler : MiniKotlinBaseVisitor<String>() {
 
     fun compile(program: MiniKotlinParser.ProgramContext, className: String = "MiniProgram"): String {
+
+        val result = "result"
+
+        println(result)
+
         return """
             public class $className {
                 public static void main(String[] args) {
