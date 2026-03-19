0e5fdf2cfd52f6deb4963de291a69ab941ad2079
1Tymon1 <tymonbielski@gmail.com>
2026-03-19 19:56:05 +0100

    Function identifiers, return types and argument lists

--- src/main/kotlin/compiler/MiniKotlinCompiler.kt
+++ src/main/kotlin/compiler/MiniKotlinCompiler.kt
@@ -7,17 +7,74 @@ class MiniKotlinCompiler : MiniKotlinBaseVisitor<String>() {
 
     fun compile(program: MiniKotlinParser.ProgramContext, className: String = "MiniProgram"): String {
 
-        val result = "result"
+        val functions = program.functionDeclaration().joinToString("\n\n")
+        {
+            visitFunction(it)
+        }
 
-        println(result)
-
-        return """
+        val result = """
             public class $className {
-                public static void main(String[] args) {
-                  return;
-                }
+                $functions
             }
         """.trimIndent()
+
+        return result
     }
 
+    fun visitFunction(function: MiniKotlinParser.FunctionDeclarationContext): String {
+        val name = function.IDENTIFIER().text
+        val returnType = convertType(function.type())
+        var arguments = ""
+
+        if (function.parameterList() != null) {
+            arguments += convertParameters(function.parameterList()) + ", Continuation<$returnType> __continuation"
+        }
+
+        if (name == "main")
+        {
+            arguments += "String[] args"
+        }
+
+        val body = visitBody(function.block())
+
+        return """public static $returnType $name($arguments){
+                $body
+            }
+        """.trimMargin()
+
+
+    }
+
+    fun convertType(type: MiniKotlinParser.TypeContext): String {
+        if (type.UNIT_TYPE() != null)
+        {
+            return "void"
+        }
+        else if (type.INT_TYPE() != null)
+        {
+            return "Integer"
+        }
+        else if (type.BOOLEAN_TYPE() != null)
+        {
+            return "Boolean"
+        }
+        else if (type.STRING_TYPE() != null)
+        {
+            return "String"
+        }
+
+        else error("unrecognized type: $type")
+    }
+
+    fun convertParameters(parameters: MiniKotlinParser.ParameterListContext): String {
+        val result = parameters.parameter().joinToString(", "){
+            "${convertType(it.type())} ${it.IDENTIFIER().text}"
+        }
+
+        return result
+    }
+
+    fun visitBody(body: MiniKotlinParser.BlockContext): String {
+        return ""
+    }
 }
